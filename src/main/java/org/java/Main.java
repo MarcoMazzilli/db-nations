3c04a4a580cd5af058a059f8e721abc3e1ec8826
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-09-29 09:25:27 +0200

    [add]db Connection + query to get all countries

--- src/main/java/org/java/Main.java
+++ src/main/java/org/java/Main.java
@@ -2,7 +2,8 @@ package org.java;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 public class Main {
 	
 	public static void main(String[] args) {
@@ -13,14 +14,38 @@ public class Main {
 		
 		try (Connection conn = DriverManager.getConnection(url, user, password)) {
 			
+			String sql = "SELECT c.country_id ,c.name,r.name ,c2.name \n"
+					+ "from countries c \n"
+					+ "JOIN regions r \n"
+					+ "	ON c.region_id = r.region_id \n"
+					+ "JOIN continents c2 \n"
+					+ "	ON c2.continent_id = r.continent_id\n"
+					+ "ORDER BY c.name \n";
+			
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet result = ps.executeQuery();
+			
+			while(result.next()) {
+				
+				int country_id = result.getInt("country_id");
+				String country = result.getString("c.name");
+				String region = result.getString("r.name");
+				String continent = result.getString("c2.name");
+				
+				System.out.println("country_id : " + country_id + "\n"
+							    +"country : " + country + "\n"
+								+"region : " + region + "\n"
+								+"continent : " + continent + "\n"
+						);
+				
+				System.out.println("\n------------------------------\n");
+			}
+			
 			System.out.println("Connessione stabilita correttamente");
 		} catch (Exception e) {
 			
 			System.out.println("Errore di connessione: " + e.getMessage());
 		}
-		
-		System.out.println("\n----------------------------------\n");
-		System.out.println("The end");
 		
 	}
 
